0367bcf385a9cd857425bdd083bd01f4919e8f69
Strider Arnor <=>
2017-03-13 15:26:43 -0500

    add put

--- server.js
+++ server.js
@@ -55,6 +55,33 @@ app.delete('/todos/:id',function(req,res){
 
 });
 
+app.put('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id, 10);
+	var body = _.pick(req.body,'completed','description');
+	var matched = _.findWhere(todos,{id: todoId});	
+	var validAttributes = {};
+
+	if(!matched){
+		return res.status(404).send();
+	}
+
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}else if(body.hasOwnProperty('description')){
+		res.status(400).send();
+	}
+
+	_.extend(matched, validAttributes);
+	res.json(matched);
+
+});
 
 app.listen(PORT, function(){
 	console.log('server started on port: '+PORT);
