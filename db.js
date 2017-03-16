8f4e9da19c760035356533771b7139cabe668069
Strider Arnor <=>
2017-03-16 15:58:48 -0500

    add postgres for prod

--- db.js
+++ db.js
@@ -1,14 +1,24 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'dev';
+var sequelize;
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
+
+
 
 var db = {};
 
-db.todo = sequelize.import(__dirname+'/models/todo.js');
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 console.log(db);
 module.exports = db;
-
